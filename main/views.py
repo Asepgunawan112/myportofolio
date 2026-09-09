5ada360acfc7a0d87988274ed9a1005849609195
Asepgunawan112 <ayyasi52@gmail.com>
2026-09-09 21:15:51 +0700

    feat: menerapkan konsep MVT pada project

---
+++ main/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from main.models import Experience
+
+# Create your views here.
+def show_main(request):
+    context = {
+        "name": "Ayyasi",
+        "npm": "2506550482",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "2nd-year Computer Science student at Universitas Indonesia."
+            "Interested in web development and currently still learning more. I"
+            "am Sundanese."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Ayyasi",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
